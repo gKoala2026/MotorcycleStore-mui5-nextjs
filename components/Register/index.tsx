6e1469e9ddc807f7a886e13a03756a7e56371118
gBrain2026 <107259158+gBrain2026@users.noreply.github.com>
2022-07-23 12:13:47 -0700

    register built

--- components/Register/index.tsx
+++ components/Register/index.tsx
@@ -1,88 +1,62 @@
-import { NextPage } from 'next'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-
-import { UserContext, IUser } from '../../context/userContext'
-import React, { useState, useEffect, useRef, useContext } from 'react';
-import validator from 'validator';
-
-import { Box, Container, Input, Stack, Typography, FormControl, styled, IconButton, FormGroup, FormControlLabel, Checkbox, } from '@mui/material'
-import InputAdornment from '@mui/material/InputAdornment';
+import { NextPage } from 'next';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { IconButton, Input, InputAdornment, Stack, styled } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { stringifyQuery } from 'next/dist/server/server-route-utils';
 
-
-
+function Copyright(props: any) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+            Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+        </Typography>
+    );
+}
 
 const ariaLabel = { 'aria-label': 'description' };
 
-interface State {
-    password: string;
-    weight: string;
-    weightRange: string;
-    showPassword: boolean;
-}
+const Register:NextPage = () => {
 
-const Login:NextPage = () => {
-
-    const router = useRouter()
-
-    // userContext
-    const userContext = useContext(UserContext)
-
-    const inputFullName = useRef()
-    const inputEmail = useRef()
-    const inputPassword = useRef()
-    const inputCPassword = useRef()
-    const [values, setValues] = useState<State>({
-        password: '',
-        weight: '',
-        weightRange: '',
-        showPassword: false,
-    });
-    const [isFName, setIsFName] = useState(true)
-    const [isEmail, setIsEmail] = useState(true)
-    const [isPwd, setIsPwd] = useState(true)
-    const [isCPwd, setIsCPwd] = useState(true)
-    const [isErr, setIsErr] = useState('')
-
-    interface ILogin {
-        email:string;
-        password:string;
-    }
-    function register() {
+    const [isErr, setIsErr] = React.useState(String)
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         setIsErr('')
-        const userEmail:any = inputEmail.current
-        const userPassword:any = inputPassword.current
-        const data : ILogin = {
-            email: userEmail.value,
-            password: userPassword.value,
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const userData = {
+            email: data.get('email'),
+            password: data.get('password'),
+            rpassword: data.get('rpassword'),
+            name: data.get('name'),
         }
-        if (validator.isEmail(data.email)==false) {
-            
-            if (data.email=='') {
-                setIsErr('Please enter email address.')
-            }
-            else setIsErr('Please enter a valid email address.')
+        console.log(userData);
+        if (userData.password!=userData.rpassword) {
+            setIsErr('rpassword')
+            return;
         }
-        else if (data.password.length<6 || data.password.length>20) {
-            if (data.password.length==0) {
-                setIsErr('Please enter password.')
-            }
-            else setIsErr('The password must be between 6-20 characters long.')
-        }
-        else {
-            let success = logIn(data)
-            console.log('succ', success)
-            return
-        }
-        return
-    }
+        
+        console.log('userData');
+    };
+    const [showPWD, setShowPWD] = React.useState(false);
+    const handleClickShowPassword = () => {
+        setShowPWD(!showPWD);
+    };
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
 
-    useEffect(() => {
-        localStorage.setItem('id', JSON.stringify(user.id))
-        console.log('kkk', localStorage.getItem('id'))
-    },[saveUser])
-    
     const ButtonBox = styled(Box)(({theme}) => ({
         marginLeft:'50px',
         borderRadius:'10px', 
@@ -94,124 +68,133 @@ const Login:NextPage = () => {
         align:'left',
         cursor:'pointer',
     }))
+  
 
-    const handleClickShowPassword = () => {
-        setValues({
-            ...values,
-            showPassword: !values.showPassword,
-        });
-    };
-    const handleChange = (event:any)  => {
-        setIsEmail(validator.isEmail(event.target.value))
-        console.log(isEmail)
-        if(event.target.value=='') setIsEmail(true)
-    };
-    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-    };
-    
     return (
-        <Container sx={{ marginTop:'100px', position:'relative'}} maxWidth={'lg'}>
-            <Box sx={{ background:"url('bg-login.png')", backgroundSize:"100% 100%", backgroundPosition:'left'}} width= {900} height= {650}>
-                <Box sx={{ background:"rgba(37, 44, 51, 0.25)", backgroundPosition:'left'}} width= {900} height= {650}> 
-                    <Stack sx={{backgroundColor:'white', borderRadius:"85px 0px 0px 85px"}} width={650} height={650} position='absolute' right='0px' alignItems='center' justifyContent='center' direction='row'>
-                        <Container maxWidth='sm'>
-                            <Stack spacing={3}>
-                                <Stack>
-                                    <Typography fontSize={32}>
-                                        Create an account
-                                    </Typography>
-                                    <Typography fontSize={18}>
-                                        Already have an account? <Link href='/Login' color='text.primary'> Login</Link> here
-                                    </Typography>
-                                </Stack>
-                                <FormControl error={isFName ? false : true}>
-                                    <Input 
-                                    inputRef={inputFullName}
-                                    type='text'
-                                    sx={{fontSize:'24px'}}
-                                    fullWidth={true}
-                                    inputProps={ariaLabel}
-                                    placeholder='Full Name'
-                                    onChange={(e) => handleChange(e)}
-                                    />
-                                </FormControl>
-                                <FormControl error={isEmail ? false : true}>
-                                    <Input 
-                                    inputRef={inputEmail}
-                                    type='email'
-                                    sx={{fontSize:'24px'}}
-                                    fullWidth={true}
-                                    inputProps={ariaLabel}
-                                    placeholder='Email'
-                                    onChange={(e) => handleChange(e)}
-                                    />
-                                </FormControl>
-                                <FormControl error={isPwd ? false : true}>
-                                    <Input 
-                                    inputRef={inputPassword}
-                                    type={values.showPassword ? 'text' : 'password'}
-                                    sx={{fontSize:'24px'}}
-                                    fullWidth={true}
-                                    inputProps={ariaLabel}
-                                    placeholder='Password'
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                            >
-                                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    }
-                                    />
-                                </FormControl>
-                                <FormControl error={isCPwd ? false : true}>
-                                    <Input 
-                                    inputRef={inputCPassword}
-                                    type={values.showPassword ? 'text' : 'password'}
-                                    sx={{fontSize:'24px'}}
-                                    fullWidth={true}
-                                    inputProps={ariaLabel}
-                                    placeholder='Password'
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                            >
-                                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    }
-                                    />
-                                </FormControl>
-                                <FormGroup>
-                                    <FormControlLabel control={<Checkbox />} label="I agree to store’s Terms and Conditions" />
-                                </FormGroup>
-                                <Stack alignItems='center'>
-                                    <Typography color='red' display={'block'}>{isErr}</Typography>
-                                    <ButtonBox width='285px' height='43px' onClick={()=>register()}>
-                                        <Typography
-                                            variant="h6"
-                                            fontSize='20px'
-                                        >
-                                            <b>Register Account</b>
-                                        </Typography>
-                                    </ButtonBox> 
-                                </Stack>
-                            </Stack>
-                        </Container>
+    
+    <Container sx={{ marginTop:'100px', position:'relative'}} maxWidth={'lg'}>
+        <Box sx={{ background:"url('bg-login.png')", backgroundSize:"100% 100%", backgroundPosition:'left'}} width= {900} height= {650}>
+            <Box sx={{ background:"rgba(37, 44, 51, 0.25)", backgroundPosition:'left'}} width= {900} height= {650}> 
+                <Stack sx={{backgroundColor:'white', borderRadius:"85px 0px 0px 85px"}} width={650} height={650} position='absolute' right='0px' alignItems='center' justifyContent='center' direction='row'>
 
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}
+                    >
+                    <Stack color="primary.main">
+                        <Typography fontSize={32}>
+                        Create an account
+                        </Typography>
+                        <Typography fontSize={18}>
+                        Already have an account? <Link href='/Login' color='text.primary'> Login</Link> here
+                        </Typography>
                     </Stack>
-                </Box>
+                    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Input 
+                            required={true}
+                            name="name"
+                            type='text'
+                            fullWidth
+                            sx={{fontSize:'24px'}}
+                            inputProps={ariaLabel}
+                            placeholder='Full Name'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Input
+                            required={true}
+                            autoFocus={true}
+                            name="email"
+                            type="email"
+                            sx={{fontSize:'24px'}}
+                            fullWidth={true}
+                            inputProps={ariaLabel}
+                            placeholder='Email'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Input
+                            required
+                            name="password"
+                            type={showPWD ? 'text' : 'password'}
+                            sx={{fontSize:'24px'}}
+                            fullWidth={true}
+                            inputProps={ariaLabel}
+                            placeholder='Password'
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {showPWD ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Input
+                            error={isErr=='rpassword' ? true : false}
+                            name="rpassword"
+                            type={showPWD ? 'text' : 'password'}
+                            sx={{fontSize:'24px'}}
+                            fullWidth={true}
+                            inputProps={ariaLabel}
+                            placeholder='Confirm Password'
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {showPWD ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControlLabel
+                            control={<Checkbox value="allowExtraEmails" color="primary" />}
+                            label="I agree to store’s Terms and Conditions"
+                            />
+                        </Grid>
+                        </Grid>
+                        <Stack alignItems='center'>
+                        <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        >
+                        Register Account
+                        </Button>
+                        </Stack>
+                        <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                            Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                        </Grid>
+                    </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 5 }} />
+                </Container>
+                </Stack>
             </Box>
-        </Container>
-        // </Box>
-    )
+        </Box>
+    </Container>
+    );
 }
 
-export default Login
+export default Register
